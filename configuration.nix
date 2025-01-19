956f5e1c52a0e4ebd1d772fcfd5ffb4624d3c4f0
Spencer Dent <mr.spencerdent@gmail.com>
2025-01-18 21:53:22 -0500

    refactor: reformat configuration

--- configuration.nix
+++ configuration.nix
@@ -11,27 +11,46 @@
 }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-      inputs.stylix.nixosModules.stylix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+    inputs.stylix.nixosModules.stylix
+  ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      # Bootloader.
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
 
-  boot.initrd.luks.devices."luks-b9ce3219-26fc-4eca-ba70-d402e918a306".device = "/dev/disk/by-uuid/b9ce3219-26fc-4eca-ba70-d402e918a306";
-  networking.hostName = "cygnus"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    initrd.luks.devices."luks-b9ce3219-26fc-4eca-ba70-d402e918a306".device =
+      "/dev/disk/by-uuid/b9ce3219-26fc-4eca-ba70-d402e918a306";
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking = {
+    hostName = "cygnus"; # Define your hostname.
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    # firewall = {
+    # Open ports in the firewall.
+    # allowedTCPPorts = [ ... ];
+    # allowedUDPPorts = [ ... ];
+
+    # Or disable the firewall altogether.
+    # enable = false;
+    # };
+
+    # Configure network proxy if necessary
+    # proxy = {
+    # default = "http://user:password@proxy:port/";
+    # noProxy = "127.0.0.1,localhost,internal.domain";
+    # };
+
+    # Enable networking
+    networkmanager.enable = true;
+
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  };
 
   # Set your time zone.
   time.timeZone = "America/New_York";
@@ -43,18 +62,20 @@
   };
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 
   programs = {
@@ -74,8 +95,17 @@
     };
 
     nm-applet.enable = true;
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # mtr.enable = true;
+    # gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
   };
 
+  # List services that you want to enable:
   services = {
     avahi = {
       enable = true;
@@ -89,47 +119,52 @@
 
     hypridle.enable = true;
 
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
+
     playerctld.enable = true;
-  };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
 
-  # Configure keymap in X11
-  services.xserver = {
-    xkb = {
-      layout = "us";
-      variant = "";
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    pulseaudio.enable = false;
+
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+
+      # Enable the GNOME Desktop Environment.
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+
+      # Enable touchpad support (enabled default in most desktopManager).
+      # libinput.enable = true;
+
+      # Configure keymap in X11
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
     };
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  services.pulseaudio.enable = false;
-
-  security.pam.services.hyprlock = {};
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  security = {
+    pam.services.hyprlock = { };
+    rtkit.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
@@ -192,24 +227,26 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment = {
-    systemPackages = [ inputs.ashell.defaultPackage.x86_64-linux ] ++ (with pkgs; [
-      brightnessctl
-      gcc
-      git
-      helix
-      hyprpaper
-      hyprpolkitagent
-      jujutsu
-      libnotify
-      networkmanagerapplet
-      nixfmt-rfc-style
-      nushell
-      mpv
-      starship
-      swaynotificationcenter
-      tealdeer
-      yazi
-    ]);
+    systemPackages =
+      [ inputs.ashell.defaultPackage.x86_64-linux ]
+      ++ (with pkgs; [
+        brightnessctl
+        gcc
+        git
+        helix
+        hyprpaper
+        hyprpolkitagent
+        jujutsu
+        libnotify
+        networkmanagerapplet
+        nixfmt-rfc-style
+        nushell
+        mpv
+        starship
+        swaynotificationcenter
+        tealdeer
+        yazi
+      ]);
 
     variables = {
       EDITOR = "hx";
@@ -261,25 +298,6 @@
 
     polarity = "dark";
   };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   xdg.portal = {
     enable = true;
