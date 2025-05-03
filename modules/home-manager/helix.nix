931b681547c36048be4b5b7ad0936946ffe1c0e6
Spencer Dent <mr.spencerdent@gmail.com>
2025-05-03 19:24:52 -0400

    refactor: extract helix home config into module

---
+++ modules/home-manager/helix.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+{
+
+  programs.helix = {
+    enable = true;
+
+    defaultEditor = true;
+
+    languages = {
+      language-server = {
+        nixd = {
+          command = "${pkgs.nixd}/bin/nixd";
+        };
+      };
+      language = [
+        {
+          name = "nix";
+          language-servers = [ "nixd" ];
+        }
+      ];
+    };
+
+    settings = {
+      editor = {
+        line-number = "relative";
+        mouse = false;
+        lsp.display-messages = true;
+
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+
+        file-picker = {
+          hidden = false;
+        };
+      };
+    };
+  };
+}
