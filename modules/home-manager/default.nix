4c30affe9e0b37a4070e48d4637a623323db620f
Spencer Dent <mr.spencerdent@gmail.com>
2025-06-18 14:17:14 -0400

    refactor: extract rmpc config into module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,6 +2,7 @@
   helix = import ./helix.nix;
   hypr = import ./hypr.nix;
   newsboat = import ./newsboat.nix;
+  rmpc = import ./rmpc.nix;
   waybar = import ./waybar.nix;
   zen-browser = import ./zen-browser.nix;
 }
