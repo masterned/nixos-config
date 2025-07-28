28aad95df3e98599984cec250ff81532f27477b0
Spencer Dent <mr.spencerdent@gmail.com>
2025-07-28 09:33:05 -0400

    feat(newsboat): set vim-like keybinds

--- modules/home-manager/newsboat.nix
+++ modules/home-manager/newsboat.nix
@@ -6,6 +6,19 @@
 
     extraConfig = ''
       macro v set browser "mpv %u" ; open-in-browser ; set browser "${pkgs.xdg-utils}/bin/xdg-open"
+
+      # unbind keys
+      unbind-key ENTER
+      unbind-key j
+      unbind-key j
+      unbind-key J
+      unbind-key K
+
+      # bind keys - vim inspired
+      bind-key j down
+      bind-key k up
+      bind-key l open
+      bind-key h quit
     '';
 
     urls = [
