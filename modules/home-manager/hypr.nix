30682703403a20d8813bf913d5a5a7fd8768ffc1
Spencer Dent <mr.spencerdent@gmail.com>
2025-10-15 11:22:56 -0400

    feat: enable & config hyprsunset

--- modules/home-manager/hypr.nix
+++ modules/home-manager/hypr.nix
@@ -139,6 +139,26 @@
     hyprpaper = {
       enable = true;
     };
+
+    hyprsunset = {
+      enable = true;
+
+      settings = {
+        max-gamma = 150;
+
+        profile = [
+          {
+            time = "7:30";
+            identity = true;
+          }
+          {
+            time = "18:00";
+            temperature = 4500;
+            gamma = 0.8;
+          }
+        ];
+      };
+    };
   };
 
   wayland.windowManager.hyprland = {
