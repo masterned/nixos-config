d9bb0e8db2d6fd6796a4b44a03af5a9ea10b77d0
Spencer Dent <mr.spencerdent@gmail.com>
2025-11-30 13:48:07 -0500

    fix: env for ssh-agent in nushell

--- home.nix
+++ home.nix
@@ -206,7 +206,7 @@
         EDITOR = "hx";
         VISUAL = "hx";
         STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$env.HOME/.steam/root/compatibilitytools.d";
-        SSH_AUTH_SOCK = "($env.XDG_RUNTIME_DIR)/ssh-agent";
+        SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
       };
 
       extraConfig = # nu
