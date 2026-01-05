621a924836e55280222140d3b376fc882507a213
Spencer Dent <mr.spencerdent@gmail.com>
2026-01-05 17:20:26 -0500

    fix: fix missing keybinds

--- modules/home-manager/hypr/default.nix
+++ modules/home-manager/hypr/default.nix
@@ -193,6 +193,18 @@ in
           "SUPER, K, movefocus, u"
           "SUPER, J, movefocus, d"
 
+          "SUPER, 1, workspace, 1"
+          "SUPER, 2, workspace, 2"
+          "SUPER, 3, workspace, 3"
+          "SUPER, 4, workspace, 4"
+          "SUPER, 5, workspace, 5"
+
+          "SUPER SHIFT, 1, movetoworkspace, 1"
+          "SUPER SHIFT, 2, movetoworkspace, 2"
+          "SUPER SHIFT, 3, movetoworkspace, 3"
+          "SUPER SHIFT, 4, movetoworkspace, 4"
+          "SUPER SHIFT, 5, movetoworkspace, 5"
+
           "SUPER, S, togglespecialworkspace, magic"
           "SUPER SHIFT, S, movetoworkspace, special:magic"
           "SUPER, G, togglegroup"
