b973e71f8b1d919bce306178f36202563b9b1270
Spencer Dent <mr.spencerdent@gmail.com>
2026-01-07 10:47:45 -0500

    wip!: update system

--- configuration.nix
+++ configuration.nix
@@ -106,7 +106,6 @@
         hyprland = {
           prettyName = "Hyprland";
           comment = "Hyprland compositor managed by UWSM";
-          binPath = "/run/current-system/sw/bin/Hyprland";
         };
       };
     };
