0af4f18b37d6f04f68c06edb5dafd2765e0889a4
Spencer Dent <mr.spencerdent@gmail.com>
2026-01-26 10:42:45 -0500

    feat: uninstall unused GNOME software

--- modules/home-manager/gnome-software.nix
+++ modules/home-manager/gnome-software.nix
@@ -1,14 +1,8 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    gnome-calculator
-    gnome-calendar
     gnome-characters
-    gnome-clocks
-    gnome-disk-utility
     gnome-font-viewer
-    gnome-text-editor
-    loupe
     nautilus
     seahorse
   ];
