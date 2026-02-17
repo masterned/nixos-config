8fee58967792f3d094fe874a69e18fca14cbf8b2
Spencer Dent <mr.spencerdent@gmail.com>
2026-02-17 08:26:42 -0500

    feat: enable flatpak

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,7 @@
     ];
     variables.EDITOR = "hx";
   };
+  services.flatpak.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
