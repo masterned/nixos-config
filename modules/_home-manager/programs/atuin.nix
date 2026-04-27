d47b709c5282af172845b5cad626c4533a56d06c
Spencer Dent <mr.spencerdent@gmail.com>
2026-04-27 14:30:28 -0400

    feat: filter atuin history

--- modules/_home-manager/programs/atuin.nix
+++ modules/_home-manager/programs/atuin.nix
@@ -5,6 +5,10 @@
     enableNushellIntegration = true;
     settings = {
       auto_sync = false;
+      history_filter = [
+        "^yy"
+        "^jj (?:ci \-m '(?:f(?:eat|ix):.*'|wip')|diff|log|ci|st)$"
+      ];
       update_check = false;
     };
   };
