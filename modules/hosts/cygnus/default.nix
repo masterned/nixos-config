6de174ea4a94c266c40eff1b2590d32d28371878
Spencer Dent <mr.spencerdent@gmail.com>
2026-05-22 22:40:27 -0400

    chore: remove temp patch in prep of fixed version

--- modules/hosts/cygnus/default.nix
+++ modules/hosts/cygnus/default.nix
@@ -21,15 +21,6 @@
             luks.devices."luks-b9ce3219-26fc-4eca-ba70-d402e918a306".device =
               "/dev/disk/by-uuid/b9ce3219-26fc-4eca-ba70-d402e918a306";
           };
-          kernelPatches = [
-            {
-              name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
-              patch = pkgs.fetchurl {
-                url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
-                hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
-              };
-            }
-          ];
         };
 
         environment.systemPackages = [ pkgs.xwayland-satellite ];
